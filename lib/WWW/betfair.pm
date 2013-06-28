4777e9e8325f1829039aafe1a0237e6a7e6eb808
David Farrell <sillymoos@cpan.org>
2013-06-28 03:16:59 +0000

    Import of SILLYMOOS/WWW-betfair-0.04 from CPAN.
    gitpan-cpan-distribution: WWW-betfair
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         SILLYMOOS/WWW-betfair-0.04.tar.gz
    gitpan-cpan-author:       SILLYMOOS
    gitpan-cpan-maturity:     released

--- lib/WWW/betfair.pm
+++ lib/WWW/betfair.pm
@@ -8,7 +8,6 @@ use Time::Piece;
 use XML::Simple;
 use Carp qw /croak/;
 use feature qw/switch/;
-use Data::Dumper;
 
 =head1 NAME
 
@@ -16,11 +15,11 @@ WWW::betfair - interact with the betfair API using OO Perl
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 
 =head1 WARNING
